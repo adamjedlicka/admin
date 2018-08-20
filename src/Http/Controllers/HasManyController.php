c0897147987d27a31b89adeb2e5f83f7fe4b0c85
Adam Jedlička <adajedlicka@gmail.com>
2018-08-20 13:47:29 +0200

    Changed to ResourceService facade

--- src/Http/Controllers/HasManyController.php
+++ src/Http/Controllers/HasManyController.php
@@ -2,27 +2,17 @@
 
 namespace AdamJedlicka\Admin\Http\Controllers;
 
-use AdamJedlicka\Admin\ResourceService;
+use AdamJedlicka\Admin\Facades\ResourceService;
 use AdamJedlicka\Admin\Serializers\IndexSerializer;
 
 class HasManyController extends Controller
 {
-    /**
-     * @var \AdamJedlicka\Admin\ResourceService
-     */
-    public $service;
-
-    public function __construct(ResourceService $service)
-    {
-        $this->service = $service;
-    }
-
     public function index(string $name, $key, string $relationship)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
         $model = $resource->model()::findOrFail($key);
         $query = $model->{$relationship}();
-        $relatedResource = $this->service->getResourceFromModel(
+        $relatedResource = ResourceService::getResourceFromModel(
             $query->getRelated()
         );
 
--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -2,10 +2,7 @@
 
 namespace AdamJedlicka\Admin\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use AdamJedlicka\Admin\Fields\Field;
-use AdamJedlicka\Admin\ResourceService;
+use AdamJedlicka\Admin\Facades\ResourceService;
 use AdamJedlicka\Admin\Serializers\ListSerializer;
 use AdamJedlicka\Admin\Serializers\IndexSerializer;
 use AdamJedlicka\Admin\Serializers\CreateSerializer;
@@ -13,16 +10,6 @@ use AdamJedlicka\Admin\Serializers\ResourceSerializer;
 
 class ResourceController extends Controller
 {
-    /**
-     * @var \AdamJedlicka\Admin\ResourceService
-     */
-    public $service;
-
-    public function __construct(ResourceService $service)
-    {
-        $this->service = $service;
-    }
-
     public function list()
     {
         return new ListSerializer();
@@ -30,26 +17,26 @@ class ResourceController extends Controller
 
     public function index(string $name)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
 
         return new IndexSerializer($resource);
     }
 
     public function create(string $name)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
 
         return new CreateSerializer($resource);
     }
 
     public function store(string $name)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
         $model = $resource->model()::make();
 
         request()->validate($resource->getCreationRules());
 
-        $model = $this->service->persistModel($resource, $model);
+        $model = ResourceService::persistModel($resource, $model);
 
         return response()->json([
             'status' => 'success',
@@ -59,7 +46,7 @@ class ResourceController extends Controller
 
     public function detail(string $name, $key)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
         $model = $resource->model()::findOrFail($key);
 
         return (new ResourceSerializer($resource, $model))
@@ -68,7 +55,7 @@ class ResourceController extends Controller
 
     public function edit(string $name, $key)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
         $model = $resource->model()::findOrFail($key);
 
         return (new ResourceSerializer($resource, $model))
@@ -77,13 +64,13 @@ class ResourceController extends Controller
 
     public function update(string $name, $key)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
 
         request()->validate($resource->getUpdateRules());
 
         $model = $resource->model()::findOrFail($key);
 
-        $this->service->persistModel($resource, $model);
+        ResourceService::persistModel($resource, $model);
 
         return response()->json([
             'status' => 'success',
@@ -92,7 +79,7 @@ class ResourceController extends Controller
 
     public function delete(string $name, $key)
     {
-        $resource = $this->service->getResourceFromName($name);
+        $resource = ResourceService::getResourceFromName($name);
 
         $model = $resource->model()::findOrFail($key);
 
