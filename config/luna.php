324967c33b9a687c2bc9c544a286340d786efcf5
Adam Jedlička <adajedlicka@gmail.com>
2018-08-31 11:25:03 +0200

    Added comments to the config file

--- config/luna.php
+++ config/luna.php
@@ -2,10 +2,26 @@
 
 return [
 
+    /**
+     * Prefix for all of Luna's URL routes
+     * and at the same time URL for Luna's home page.
+     *
+     * Default: luna
+     */
     'prefix' => 'luna',
 
+    /**
+     * Directory name where all of site's Luna files are located.
+     *
+     * Default: luna
+     */
     'directory' => 'Luna',
 
+    /**
+     * Middleware applied to all of Luna's routes.
+     *
+     * Default: ['web', 'auth']
+     */
     'middleware' => [
         'web',
         'auth',
